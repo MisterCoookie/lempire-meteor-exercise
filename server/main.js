65966748c70a5cd5793b449a93f247a85077e926
ashDevlop <elisabeth.nathanael.gaming@gmail.com>
2023-01-27 00:13:10 +0100

    [fix] fix commentary

--- server/main.js
+++ server/main.js
@@ -1,20 +1,14 @@
+// Meteor imports
 import { Meteor } from 'meteor/meteor';
-import { ExportCollection } from "../imports/db/export.collection.js";
+
+// Internal imports
 import '../imports/api/export.methods.js'
 import '../imports/api/export.publication.js'
 
 Meteor.startup(() => {
-    console.log(ExportCollection.find().count())
-    if (ExportCollection.find().count() === 1) {
-        console.log('There is nothing')
 
-        ExportCollection.insert({
-            name: 'randomExport2.png',
-            state: 'sandbox',
-            percent: 0
-        })
-    }
-
+    // I set up a fun interval to simulate the management of the export by the server
+    // So every seconds the server check to see if any export items need treatment.
     Meteor.setInterval(() => {
         Meteor.call('exports.progressExporting')
     }, 1000)
